84f6c3a2906014848137c57284a35a8432f19fc8
DDC <diaz_carrete@yahoo.com>
2017-08-11 23:57:14 +0200

    Some basic tests.

--- streamy-testsuite/lib/Test/Common.hs
+++ streamy-testsuite/lib/Test/Common.hs
@@ -1,3 +1,30 @@
-module Test.Common where
+module Test.Common (common) where
 
-import Test.Common.Streamy
+import Test.Tasty (TestTree)
+import Test.Tasty.HUnit (testCase,Assertion,assertEqual,assertBool)
+
+import Test.Common.Streamy (Stream)
+import qualified Test.Common.Streamy as Y
+
+import Control.Monad.IO.Class
+import Control.Concurrent.MVar
+
+common :: [TestTree]
+common = 
+    [ testCase "basic" basic 
+    ]
+
+basic :: Assertion
+basic = do
+   ref <- newMVar ""
+   let addChar c = modifyMVar_ ref (return . (c:))
+       stream = Y.yield 'a' *> Y.yield 'b' *> Y.yield 'c' *> liftIO (addChar 'd') *> return True
+   b <- Y.effects . Y.chain addChar $ stream 
+   acc <- readMVar ref
+   assertBool "Stream returned unexpectd value." b
+   assertEqual "Wrong accumulator value" "abcd" (reverse acc)
+
+
+
+
+
--- streamy-testsuite/tests/tests.hs
+++ streamy-testsuite/tests/tests.hs
@@ -1,16 +1,22 @@
 module Main where
 
 import Test.Tasty
-import Test.Tasty.HUnit
 
 import Test.Pipes.Common
+import Test.Streaming.Common
+import Test.Conduit.Common
+
 
 main :: IO ()
 main = defaultMain tests
 
 tests :: TestTree
 tests = 
-    testGroup "Tests" 
-    []
-
+    testGroup "All" 
+    [ testGroup "Common" 
+      [ testGroup "Pipes" Test.Pipes.Common.common
+      , testGroup "Streaming" Test.Streaming.Common.common
+      , testGroup "Conduit" Test.Conduit.Common.common
+      ]
+    ]
 
