51cfa82ce22c8c4f39bcf59ffca54574a54c0018
DDC <diaz_carrete@yahoo.com>
2017-08-11 19:26:56 +0200

    Added effects to the sig.

--- streamy-pipes/lib/Streamy/Pipes.hs
+++ streamy-pipes/lib/Streamy/Pipes.hs
@@ -11,3 +11,6 @@ yield = P.yield
 
 chain :: Monad m => (o -> m ()) -> Stream o m r -> Stream o m r
 chain action producer = producer >-> PP.chain action
+
+effects :: Monad m => Stream o m r -> m r
+effects producer = P.runEffect $ producer >-> PP.drain   
