c01a45428b71281ba7d50b76e65af28ef7cdf82e
DDC <diaz_carrete@yahoo.com>
2017-08-11 02:05:42 +0200

    Added chain.

--- streamy-pipes/lib/Streamy/Pipes.hs
+++ streamy-pipes/lib/Streamy/Pipes.hs
@@ -1,11 +1,13 @@
-module Streamy.Pipes (Stream,yield) where
+module Streamy.Pipes (Stream,yield,chain) where
 
-import Pipes (Proxy,X)
+import Pipes (Proxy,X,(>->))
 import qualified Pipes as P
+import qualified Pipes.Prelude as PP
 
 type Stream = Proxy X () ()
 
 yield :: Monad m => o -> Stream o m ()
 yield = P.yield
 
-
+chain :: Monad m => (o -> m ()) -> Stream o m r -> Stream o m r
+chain action producer = producer >-> PP.chain action
